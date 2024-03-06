966142aa820de2069ae6b637815c4092e1e783cf
Maksim <madzarmaksim@gmail.com>
2024-03-06 15:43:43 +0100

    implement login

--- backend/src/main/kotlin/org/gymapp/backend/security/service/impl/SecurityServiceImpl.kt
+++ backend/src/main/kotlin/org/gymapp/backend/security/service/impl/SecurityServiceImpl.kt
@@ -1,28 +1,35 @@
 package org.gymapp.backend.security.service.impl
 
+import org.gymapp.backend.security.exception.UserNotFoundException
 import org.gymapp.backend.security.model.SecurityUser
 import org.gymapp.backend.security.repository.SecurityUserRepository
+import org.gymapp.backend.security.service.JwtService
 import org.gymapp.backend.security.service.SecurityService
 import org.gymapp.library.request.CreateAccountRequest
 import org.gymapp.library.response.SecurityUserDto
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Service
 
 @Service
 class SecurityServiceImpl(
     private val securityUserRepository: SecurityUserRepository,
-): SecurityService {
+    private val authenticationManager: AuthenticationManager,
+    private val jwtService: JwtService,
+    private val passwordEncoder: BCryptPasswordEncoder,
+) : SecurityService {
 
     override fun register(request: CreateAccountRequest): ResponseEntity<Void> {
-
         if (securityUserRepository.existsByUsername(request.username)) {
             return ResponseEntity.status(HttpStatus.CONFLICT).build()
         }
 
         val securityUser = SecurityUser(
             username = request.username,
-            password = request.password,
+            password = passwordEncoder.encode(request.password),
             role = request.role
         )
 
@@ -30,11 +37,17 @@ class SecurityServiceImpl(
         return ResponseEntity.status(HttpStatus.CREATED).build()
     }
 
-    override fun login(email: String, password: String): ResponseEntity<Map<String, String>> {
-        TODO("Not yet implemented")
+    override fun login(username: String, password: String): ResponseEntity<Map<String, String>> {
+        val securityUser = securityUserRepository.findByUsername(username)
+            .orElseThrow { UserNotFoundException("User not found!") }
+
+        authenticationManager.authenticate(UsernamePasswordAuthenticationToken(username, password))
+
+        val token = jwtService.generateAccessToken(securityUser)
+        return ResponseEntity.ok(mapOf("accessToken" to token))
     }
 
-    override fun updateRoles(email: String, role: String): ResponseEntity<Void> {
+    override fun updateRoles(username: String, role: String): ResponseEntity<Void> {
         TODO("Not yet implemented")
     }
 
