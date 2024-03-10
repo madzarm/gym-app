b508b32b816675daa25dd911c189d5937257ad1b
Maksim <madzarmaksim@gmail.com>
2024-03-10 16:37:04 +0100

    add logam font

---
+++ gym-app/app/src/main/java/com/example/gym_app/ui/theme/Font.kt
@@ -0,0 +1,9 @@
+package com.example.gym_app.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import com.example.gym_app.R
+
+val logam = FontFamily(
+    Font(R.font.logam)
+)
