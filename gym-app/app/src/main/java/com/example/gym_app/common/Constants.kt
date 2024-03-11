d5e33dcfa19e891ae4587823df06d7c49571b259
Maksim <madzarmaksim@gmail.com>
2024-03-11 10:04:33 +0100

    add constants

---
+++ gym-app/app/src/main/java/com/example/gym_app/common/Constants.kt
@@ -0,0 +1,12 @@
+package com.example.gym_app.common
+
+
+enum class Role {
+    OWNER, MEMBER, TRAINER
+}
+
+object AppRoutes {
+    const val WELCOME_SCREEN = "WelcomeScreen"
+    const val SIGNUP_ROLE_SELECTION_SCREEN = "SignupRoleSelectionScreen"
+    const val CREATE_ACCOUNT_SCREEN = "CreateAccountScreen"
+}
