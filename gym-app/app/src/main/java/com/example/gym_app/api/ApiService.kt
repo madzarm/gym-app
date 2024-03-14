ff3bfae0ec5746538d4963cc01387ef485e36681
Maksim <madzarmaksim@gmail.com>
2024-03-14 18:54:30 +0100

    add authorization header

--- gym-app/app/src/main/java/com/example/gym_app/api/ApiService.kt
+++ gym-app/app/src/main/java/com/example/gym_app/api/ApiService.kt
@@ -3,9 +3,10 @@ package com.example.gym_app.api
 import org.gymapp.library.request.CreateUserRequest
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface ApiService {
     @POST("/user")
-    suspend fun register(@Body request: CreateUserRequest): Response<Void>
+    suspend fun createUser(@Body request: CreateUserRequest, @Header("Authorization") authHeader: String): Response<Void>
 }
