de42e372614cf622193fca04c4850eae47aa1381
Maksim <madzarmaksim@gmail.com>
2024-03-14 11:35:49 +0100

    change security config

--- backend/src/main/kotlin/org/gymapp/backend/security/config/WebConfig.kt
+++ backend/src/main/kotlin/org/gymapp/backend/security/config/WebConfig.kt
@@ -1,20 +1,20 @@
 package org.gymapp.backend.security.config
 
-import org.gymapp.backend.security.filters.JwtAuthenticationFilter
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.invoke
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter
 import org.springframework.security.web.SecurityFilterChain
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(securedEnabled = true, prePostEnabled = true, jsr250Enabled = true)
 class WebConfig(
-    private val jwtAuthenticationFilter: JwtAuthenticationFilter,
 ) {
 
     @Bean
@@ -22,14 +22,44 @@ class WebConfig(
         http.invoke {
             csrf { disable() }
             authorizeHttpRequests {
-                authorize("/greetings/**", hasAuthority("ROLE_ADMIN"))
-                authorize("/test/**", hasAuthority("ROLE_ADMIN"))
-                authorize("/**", permitAll)
+                authorize("/user", authenticated)
             }
             apply {
-                addFilterBefore<UsernamePasswordAuthenticationFilter>(jwtAuthenticationFilter)
+                oauth2ResourceServer {
+                    jwt {
+                        jwtAuthenticationConverter()
+                    }
+                }
             }
         }
         return http.build()
     }
+
+    fun jwtAuthenticationConverter(): JwtAuthenticationConverter {
+        val converter = JwtAuthenticationConverter()
+        return converter
+    }
 }
+
+@Configuration
+class JwtDecoderConfig {
+
+    @Value("\${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+    private lateinit var jwkSetUri: String
+
+    @Bean
+    fun jwtDecoder(): NimbusJwtDecoder {
+        return NimbusJwtDecoder.withJwkSetUri(this.jwkSetUri).build()
+    }
+}
+
+//class CustomJwtAuthenticationConverter(
+//    private val userDetailsService: CustomUserDetailsService
+//) : Converter<Jwt, AbstractAuthenticationToken> {
+//
+//    override fun convert(jwt: Jwt): AbstractAuthenticationToken {
+//        val user = userDetailsService.loadUserByUsername(jwt.claimAsString("email")) // Assuming 'email' is a claim
+//        val authorities = user.roles.map { SimpleGrantedAuthority("ROLE_${it.name}") } // Customize this based on your role structure
+//        return UsernamePasswordAuthenticationToken(user, jwt, authorities)
+//    }
+//}
