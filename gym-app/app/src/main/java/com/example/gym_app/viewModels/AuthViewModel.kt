75562ac1edb935e41763ae1b460e73e4340dd554
Maksim <madzarmaksim@gmail.com>
2024-03-14 11:37:50 +0100

    add user state

--- gym-app/app/src/main/java/com/example/gym_app/viewModels/AuthViewModel.kt
+++ gym-app/app/src/main/java/com/example/gym_app/viewModels/AuthViewModel.kt
@@ -2,13 +2,21 @@ package com.example.gym_app.viewModels
 
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import org.gymapp.library.request.CreateUserRequest
 
 class AuthViewModel : ViewModel() {
 
-    private val _token = mutableStateOf("")
-    val token = _token
+    private val _accessToken = mutableStateOf("")
+    val accessToken = _accessToken
+
+    private val _userState = mutableStateOf<CreateUserRequest?>(null)
+    val userState = _userState
 
     fun setToken(token: String) {
-        _token.value = token
+        _accessToken.value = token
+    }
+
+    fun createUser(state: CreateUserRequest) {
+        _userState.value = state
     }
 }
