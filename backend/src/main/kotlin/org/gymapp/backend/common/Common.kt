1884c2abd424b77ece595551bbf44c0ac962ee2b
Maksim <madzarmaksim@gmail.com>
2024-03-19 11:32:38 +0100

    make common bean

--- backend/src/main/kotlin/org/gymapp/backend/common/Common.kt
+++ backend/src/main/kotlin/org/gymapp/backend/common/Common.kt
@@ -1,9 +1,19 @@
 package org.gymapp.backend.common
 
+import org.gymapp.backend.model.User
+import org.gymapp.backend.repository.UserRepository
+import org.gymapp.backend.security.exception.UserNotFoundException
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.oauth2.jwt.Jwt
+import org.springframework.stereotype.Component
 import kotlin.random.Random
 
-object Common {
+@Component
+class Common(@Autowired val userRepository: UserRepository) {
+
+    enum class Roles {
+        ROLE_ADMIN, ROLE_MEMBER, ROLE_TRAINER
+    }
 
     fun extractId(jwt: Jwt) = jwt.getClaimAsString("sub").split("|")[1]
 
@@ -12,8 +22,13 @@ object Common {
         return String.format("%04d", randomNumber)
     }
 
+    fun getCurrentUser(jwt: Jwt): User {
+        val id = extractId(jwt)
+        return userRepository.findById(id).orElseThrow {
+            UserNotFoundException("User not found")
+        }
+    }
+
+
 }
 
-enum class Roles {
-    ROLE_ADMIN, ROLE_MEMBER, ROLE_TRAINER
-}
