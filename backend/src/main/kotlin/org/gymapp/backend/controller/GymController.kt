8c3ea2b7e22fc394cbbbc020e1a639b9512e5fd6
Maksim <madzarmaksim@gmail.com>
2024-03-19 11:36:44 +0100

    refactor

--- backend/src/main/kotlin/org/gymapp/backend/controller/GymController.kt
+++ backend/src/main/kotlin/org/gymapp/backend/controller/GymController.kt
@@ -3,6 +3,7 @@ package org.gymapp.backend.controller
 import org.gymapp.backend.common.Common
 import org.gymapp.backend.service.GymService
 import org.gymapp.library.request.CreateGymRequest
+import org.gymapp.library.response.GymDto
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
@@ -20,12 +21,12 @@ class GymController(
     fun createGym(
         @RequestBody request: CreateGymRequest,
         @AuthenticationPrincipal jwt: Jwt
-    ): ResponseEntity<*> {
+    ): ResponseEntity<GymDto> {
         return ResponseEntity.status(201).body(gymService.createGym(request, jwt))
     }
 
     @GetMapping
-    fun findUserGyms(@AuthenticationPrincipal jwt: Jwt): ResponseEntity<*> {
+    fun findUserGyms(@AuthenticationPrincipal jwt: Jwt): ResponseEntity<List<GymDto>> {
         return ResponseEntity.ok(gymService.findUserGyms(common.getCurrentUser(jwt)))
     }
 
