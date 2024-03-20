b7255b98838a689fb1ec8186bef4043bb31611b0
Maksim <madzarmaksim@gmail.com>
2024-03-20 21:33:32 +0100

    optimize encoding algorithm

--- gym-app/app/src/main/java/com/example/gym_app/common/Common.kt
+++ gym-app/app/src/main/java/com/example/gym_app/common/Common.kt
@@ -6,6 +6,7 @@ import android.graphics.BitmapFactory
 import android.graphics.ImageDecoder
 import android.net.Uri
 import android.os.Build
+import android.provider.MediaStore
 import android.util.Base64
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
@@ -14,6 +15,7 @@ import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import org.gymapp.library.response.ExceptionResult
 import retrofit2.HttpException
 import java.io.ByteArrayOutputStream
+import kotlin.math.max
 
 fun base64StringToImageBitmap(base64String: String): ImageBitmap {
     val imageBytes = Base64.decode(base64String, Base64.DEFAULT)
@@ -34,17 +36,30 @@ fun readErrorMessage(e: HttpException): String {
     return errorMessage;
 }
 
-fun uriToBase64(context: Context, uri: Uri): String {
+fun uriToBase64(context: Context, uri: Uri, maxWidth: Int = 250, maxHeight: Int = 250): String {
     val bitmap = if (Build.VERSION.SDK_INT < 28) {
         @Suppress("DEPRECATION")
-        android.provider.MediaStore.Images.Media.getBitmap(context.contentResolver, uri)
+        MediaStore.Images.Media.getBitmap(context.contentResolver, uri)
     } else {
         val source = ImageDecoder.createSource(context.contentResolver, uri)
         ImageDecoder.decodeBitmap(source)
     }
 
+    val scaleFactor = max(
+        1.0f,
+        max(bitmap.width / maxWidth.toFloat(), bitmap.height / maxHeight.toFloat())
+    )
+
+    val resizedBitmap = Bitmap.createScaledBitmap(
+        bitmap,
+        (bitmap.width / scaleFactor).toInt(),
+        (bitmap.height / scaleFactor).toInt(),
+        true
+    )
+
     val outputStream = ByteArrayOutputStream()
-    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+    resizedBitmap.compress(Bitmap.CompressFormat.JPEG, 50, outputStream)
     val imageBytes = outputStream.toByteArray()
+
     return Base64.encodeToString(imageBytes, Base64.DEFAULT)
 }
