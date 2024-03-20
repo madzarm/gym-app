5f3bc9abb08cd8e174f6ad4257f8069b0e07cc06
Maksim <madzarmaksim@gmail.com>
2024-03-20 10:16:56 +0100

    add okHttpClient logging interceptor

--- gym-app/app/src/main/java/com/example/gym_app/api/ApiClient.kt
+++ gym-app/app/src/main/java/com/example/gym_app/api/ApiClient.kt
@@ -1,13 +1,25 @@
 package com.example.gym_app.api
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://192.168.110.195:8080/"
+    private const val BASE_URL = "http://192.168.237.195:8080/"
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        })
+        .build()
 
     val retrofit: Retrofit by lazy {
-        Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build()
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
     }
 }
 
