a6186ff46e60c0b601da5ed23cb1de7ccd2a5c74
Maksim <madzarmaksim@gmail.com>
2024-03-20 10:16:37 +0100

    add api calls

--- gym-app/app/src/main/java/com/example/gym_app/api/ApiService.kt
+++ gym-app/app/src/main/java/com/example/gym_app/api/ApiService.kt
@@ -1,12 +1,21 @@
 package com.example.gym_app.api
 
 import org.gymapp.library.request.CreateUserRequest
+import org.gymapp.library.response.GymUserDto
+import org.gymapp.library.response.UserDto
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface ApiService {
-    @POST("/user")
+    @POST("/users")
     suspend fun createUser(@Body request: CreateUserRequest, @Header("Authorization") authHeader: String): Response<Void>
+
+    @GET("/users/current")
+    suspend fun getCurrentUser(@Header("Authorization") authHeader: String): Response<UserDto>
+
+   @GET("/users/gyms")
+    suspend fun getUserGyms(@Header("Authorization") authHeader: String): Response<List<GymUserDto>>
 }
