502d3d44ec4a3dedbe8804e996147459476d0ef6
Maksim <madzarmaksim@gmail.com>
2024-03-30 13:48:30 +0100

    fix access code mapper

--- backend/src/main/kotlin/org/gymapp/backend/mapper/AccessCodeMapper.java
+++ backend/src/main/kotlin/org/gymapp/backend/mapper/AccessCodeMapper.java
@@ -3,10 +3,12 @@ package org.gymapp.backend.mapper;
 import org.gymapp.backend.model.AccessCode;
 import org.gymapp.backend.model.AccessCodeDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface AccessCodeMapper {
 
+    @Mapping(source = "gym.id", target = "gymId")
     AccessCodeDto modelToDto(AccessCode accessCode);
 
 }
