9ce564db23faeb35635ffb247b7222f27adc737a
Maksim <madzarmaksim@gmail.com>
2024-03-30 13:49:54 +0100

    add 2 endpoints

--- gym-app/app/src/main/java/com/example/gym_app/api/ApiService.kt
+++ gym-app/app/src/main/java/com/example/gym_app/api/ApiService.kt
@@ -2,6 +2,7 @@ package com.example.gym_app.api
 
 import org.gymapp.library.request.CreateGymRequest
 import org.gymapp.library.request.CreateUserRequest
+import org.gymapp.library.response.AccessCodeDto
 import org.gymapp.library.response.GymUserDto
 import org.gymapp.library.response.UserDto
 import retrofit2.Response
@@ -9,6 +10,7 @@ import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiService {
@@ -23,6 +25,12 @@ interface ApiService {
 
     @POST("/users/join-as-member")
     suspend fun joinGymAsMember(@Header("Authorization") authHeader: String, @Query("code") code: String): GymUserDto
+
+    @POST("/users/join-as-trainer")
+    suspend fun joinGymAsTrainer(@Header("Authorization") authHeader: String, @Query("code") code: String): GymUserDto
+
+    @GET("/gyms/{id}/accessCode")
+    suspend fun generateAccessCode(@Header("Authorization") authHeader: String, @Path("id") gymId: String): AccessCodeDto
 
     @POST("/gyms")
     suspend fun createGym(@Header("Authorization") authHeader: String, @Body request: CreateGymRequest): GymUserDto
