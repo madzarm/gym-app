ad174e44dd2fde6bff6abf81e04b85fb71c6c15a
Maksim <madzarmaksim@gmail.com>
2024-04-03 20:08:16 +0200

    add fcm token

--- backend/src/main/kotlin/org/gymapp/backend/model/User.kt
+++ backend/src/main/kotlin/org/gymapp/backend/model/User.kt
@@ -14,6 +14,7 @@ class User(
     var profilePicUrl: String?,
     var createdAt: LocalDateTime?,
     var updatedAt: LocalDateTime?,
+    var fcmToken: String? = null,
     @OneToMany(mappedBy = "user") val gymUsers: MutableList<GymUser> = mutableListOf()
 ) {
 
