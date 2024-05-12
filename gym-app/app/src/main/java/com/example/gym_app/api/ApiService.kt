4286e47c70d7238b5ca4c753b06b3c3676c16e80
Maksim <madzarmaksim@gmail.com>
2024-05-12 19:52:46 +0200

    reformat code

--- gym-app/app/src/main/java/com/example/gym_app/api/ApiService.kt
+++ gym-app/app/src/main/java/com/example/gym_app/api/ApiService.kt
@@ -37,114 +37,229 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiService {
-    @POST("/users")
-    suspend fun createUser(@Body request: CreateUserRequest, @Header("Authorization") authHeader: String): Response<Void>
+  @POST("/users")
+  suspend fun createUser(
+    @Body request: CreateUserRequest,
+    @Header("Authorization") authHeader: String,
+  ): Response<Void>
 
-    @PUT("/users")
-    suspend fun updateUser(@Header("Authorization") authHeader: String, @Body request: CreateUserRequest): Response<Void>
+  @PUT("/users")
+  suspend fun updateUser(
+    @Header("Authorization") authHeader: String,
+    @Body request: CreateUserRequest,
+  ): Response<Void>
 
-    @GET("/users/current")
-    suspend fun getCurrentUser(@Header("Authorization") authHeader: String): Response<UserDto>
+  @GET("/users/current")
+  suspend fun getCurrentUser(@Header("Authorization") authHeader: String): Response<UserDto>
 
-   @GET("/users/gyms")
-    suspend fun getUserGyms(@Header("Authorization") authHeader: String): Response<List<GymUserDto>>
+  @GET("/users/gyms")
+  suspend fun getUserGyms(@Header("Authorization") authHeader: String): Response<List<GymUserDto>>
 
-    @POST("/users/join-as-member")
-    suspend fun joinGymAsMember(@Header("Authorization") authHeader: String, @Query("code") code: String): GymUserDto
+  @POST("/users/join-as-member")
+  suspend fun joinGymAsMember(
+    @Header("Authorization") authHeader: String,
+    @Query("code") code: String,
+  ): GymUserDto
 
-    @POST("/users/join-as-member/invite/")
-    suspend fun joinGymWithFriendCode(@Header("Authorization") authHeader: String, @Query("code") code: String): GymUserDto
+  @POST("/users/join-as-member/invite/")
+  suspend fun joinGymWithFriendCode(
+    @Header("Authorization") authHeader: String,
+    @Query("code") code: String,
+  ): GymUserDto
 
-    @POST("/users/join-as-trainer")
-    suspend fun joinGymAsTrainer(@Header("Authorization") authHeader: String, @Query("code") code: String): GymUserDto
+  @POST("/users/join-as-trainer")
+  suspend fun joinGymAsTrainer(
+    @Header("Authorization") authHeader: String,
+    @Query("code") code: String,
+  ): GymUserDto
 
-    @GET("owners/gyms/{id}/accessCode")
-    suspend fun generateAccessCode(@Header("Authorization") authHeader: String, @Path("id") gymId: String): AccessCodeDto
+  @GET("owners/gyms/{id}/accessCode")
+  suspend fun generateAccessCode(
+    @Header("Authorization") authHeader: String,
+    @Path("id") gymId: String,
+  ): AccessCodeDto
 
-    @POST("/gyms")
-    suspend fun createGym(@Header("Authorization") authHeader: String, @Body request: CreateGymRequest): GymUserDto
+  @POST("/gyms")
+  suspend fun createGym(
+    @Header("Authorization") authHeader: String,
+    @Body request: CreateGymRequest,
+  ): GymUserDto
 
-    @GET("/trainers/gyms/{gymId}")
-    suspend fun getTrainer(@Header("Authorization") authHeader: String, @Path("gymId") gymId: String): GymTrainerDto
+  @GET("/trainers/gyms/{gymId}")
+  suspend fun getTrainer(
+    @Header("Authorization") authHeader: String,
+    @Path("gymId") gymId: String,
+  ): GymTrainerDto
 
-    @POST("/gyms/members")
-    suspend fun getGymMembers(@Header("Authorization") authHeader: String, @Body memberIds: List<String>): List<GymMemberDtoFull>
+  @POST("/gyms/members")
+  suspend fun getGymMembers(
+    @Header("Authorization") authHeader: String,
+    @Body memberIds: List<String>,
+  ): List<GymMemberDtoFull>
 
-    @GET("/trainers/gyms/{gymId}/upcoming-classes")
-    suspend fun getTrainerWithUpcomingClasses(@Header("Authorization") authHeader: String, @Path("gymId") gymId: String): GymTrainerDto
+  @GET("/trainers/gyms/{gymId}/upcoming-classes")
+  suspend fun getTrainerWithUpcomingClasses(
+    @Header("Authorization") authHeader: String,
+    @Path("gymId") gymId: String,
+  ): GymTrainerDto
 
-    @PUT("/trainers/gyms/classes/{classId}")
-    suspend fun updateGymClass(@Header("Authorization") authHeader: String, @Path("classId") classId: String, @Body request: UpdateClassRequest): GymTrainerDto
+  @PUT("/trainers/gyms/classes/{classId}")
+  suspend fun updateGymClass(
+    @Header("Authorization") authHeader: String,
+    @Path("classId") classId: String,
+    @Body request: UpdateClassRequest,
+  ): GymTrainerDto
 
-    @PUT("/trainers/gyms/classes/{classId}/instances")
-    suspend fun updateRecurringGymClass(@Header("Authorization") authHeader: String, @Path("classId") classId: String, @Body request: UpdateGymClassInstanceRequest): GymTrainerDto
+  @PUT("/trainers/gyms/classes/{classId}/instances")
+  suspend fun updateRecurringGymClass(
+    @Header("Authorization") authHeader: String,
+    @Path("classId") classId: String,
+    @Body request: UpdateGymClassInstanceRequest,
+  ): GymTrainerDto
 
-    @DELETE("/trainers/gyms/classes/{classId}")
-    suspend fun deleteGymClass(@Header("Authorization") authHeader: String, @Path("classId") classId: String): GymTrainerDto
+  @DELETE("/trainers/gyms/classes/{classId}")
+  suspend fun deleteGymClass(
+    @Header("Authorization") authHeader: String,
+    @Path("classId") classId: String,
+  ): GymTrainerDto
 
-    @DELETE("/trainers/gyms/classes/{classId}/cancel")
-    suspend fun cancelGymClass(@Header("Authorization") authHeader: String, @Path("classId") classId: String, @Query("dateTime") dateTime: String): GymTrainerDto
+  @DELETE("/trainers/gyms/classes/{classId}/cancel")
+  suspend fun cancelGymClass(
+    @Header("Authorization") authHeader: String,
+    @Path("classId") classId: String,
+    @Query("dateTime") dateTime: String,
+  ): GymTrainerDto
 
-    @POST("/trainers/gyms/{gymId}/classes")
-    suspend fun createGymClass(@Header("Authorization") authHeader: String, @Path("gymId") gymId: String, @Body request: UpdateClassRequest): GymTrainerDto
+  @POST("/trainers/gyms/{gymId}/classes")
+  suspend fun createGymClass(
+    @Header("Authorization") authHeader: String,
+    @Path("gymId") gymId: String,
+    @Body request: UpdateClassRequest,
+  ): GymTrainerDto
 
-    @POST("/trainers/gyms/{gymId}/classes/recurring")
-    suspend fun createRecurringGymClass(@Header("Authorization") authHeader: String, @Path("gymId") gymId: String, @Body request: CreateRecurringClassRequest): GymTrainerDto
+  @POST("/trainers/gyms/{gymId}/classes/recurring")
+  suspend fun createRecurringGymClass(
+    @Header("Authorization") authHeader: String,
+    @Path("gymId") gymId: String,
+    @Body request: CreateRecurringClassRequest,
+  ): GymTrainerDto
 
-    @GET("/gyms/{gymId}/classes")
-    suspend fun getUpcomingGymClasses(@Header("Authorization") authHeader: String, @Path("gymId") gymId: String): List<GymClassDto>
+  @GET("/gyms/{gymId}/classes")
+  suspend fun getUpcomingGymClasses(
+    @Header("Authorization") authHeader: String,
+    @Path("gymId") gymId: String,
+  ): List<GymClassDto>
 
-    @POST("/members/classes/{classId}")
-    suspend fun registerToClass(@Header("Authorization") authHeader: String, @Path("classId") classId: String, @Query("dateTime") dateTime: String): GymMemberDto
+  @POST("/members/classes/{classId}")
+  suspend fun registerToClass(
+    @Header("Authorization") authHeader: String,
+    @Path("classId") classId: String,
+    @Query("dateTime") dateTime: String,
+  ): GymMemberDto
 
-    @GET("/gyms/{gymId}/live")
-    suspend fun getLiveStatus(@Header("Authorization") authHeader: String, @Path("gymId") gymId: String): Int
+  @GET("/gyms/{gymId}/live")
+  suspend fun getLiveStatus(
+    @Header("Authorization") authHeader: String,
+    @Path("gymId") gymId: String,
+  ): Int
 
-    @GET("/gyms/{gymId}/gymVisits")
-    suspend fun getGymVisits(@Header("Authorization") authHeader: String, @Path("gymId") gymId: String): List<GymVisitDto>
+  @GET("/gyms/{gymId}/gymVisits")
+  suspend fun getGymVisits(
+    @Header("Authorization") authHeader: String,
+    @Path("gymId") gymId: String,
+  ): List<GymVisitDto>
 
-    @POST("/members/gyms/review-class")
-    suspend fun reviewGymClass(@Header("Authorization") authHeader: String, @Body request: ReviewGymClassRequest): GymClassReviewDto
+  @POST("/members/gyms/review-class")
+  suspend fun reviewGymClass(
+    @Header("Authorization") authHeader: String,
+    @Body request: ReviewGymClassRequest,
+  ): GymClassReviewDto
 
-    @POST("/members/gyms/review-trainer")
-    suspend fun reviewTrainer(@Header("Authorization") authHeader: String, @Body request: ReviewTrainerRequest): GymTrainerReviewDto
+  @POST("/members/gyms/review-trainer")
+  suspend fun reviewTrainer(
+    @Header("Authorization") authHeader: String,
+    @Body request: ReviewTrainerRequest,
+  ): GymTrainerReviewDto
 
-    @GET("/members/gyms/{gymId}/classes-for-review")
-    suspend fun getGymClassesForReview(@Header("Authorization") authHeader: String, @Path("gymId") gymId: String): List<GymClassInstanceDto>
+  @GET("/members/gyms/{gymId}/classes-for-review")
+  suspend fun getGymClassesForReview(
+    @Header("Authorization") authHeader: String,
+    @Path("gymId") gymId: String,
+  ): List<GymClassInstanceDto>
 
-    @GET("/owners/gyms/{gymId}/trainers")
-    suspend fun getTrainersWithReviews(@Header("Authorization") authHeader: String, @Path("gymId") gymId: String): List<GymTrainerWithReviewsDto>
+  @GET("/owners/gyms/{gymId}/trainers")
+  suspend fun getTrainersWithReviews(
+    @Header("Authorization") authHeader: String,
+    @Path("gymId") gymId: String,
+  ): List<GymTrainerWithReviewsDto>
 
-    @GET("/owners/gyms/{gymId}/classes")
-    suspend fun getGymClassesWithReviews(@Header("Authorization") authHeader: String, @Path("gymId") gymId: String): List<GymClassWithReviewsDto>
+  @GET("/owners/gyms/{gymId}/classes")
+  suspend fun getGymClassesWithReviews(
+    @Header("Authorization") authHeader: String,
+    @Path("gymId") gymId: String,
+  ): List<GymClassWithReviewsDto>
 
-    @GET("/challenges")
-    suspend fun fetchActiveChallenges(@Header("Authorization") authHeader: String, @Query("gymId") gymId: String): List<ChallengeDto>
+  @GET("/challenges")
+  suspend fun fetchActiveChallenges(
+    @Header("Authorization") authHeader: String,
+    @Query("gymId") gymId: String,
+  ): List<ChallengeDto>
 
-    @POST("/challenges/timed-visit-based")
-    suspend fun createTimedBasedChallenge(@Header("Authorization") authHeader: String, @Query("gymId") gymId: String, @Body request: CreateTimedVisitBasedChallengeRequest): Response<Unit>
+  @POST("/challenges/timed-visit-based")
+  suspend fun createTimedBasedChallenge(
+    @Header("Authorization") authHeader: String,
+    @Query("gymId") gymId: String,
+    @Body request: CreateTimedVisitBasedChallengeRequest,
+  ): Response<Unit>
 
-    @POST("/challenges/frequency-based")
-    suspend fun createFrequencyBasedChallenge(@Header("Authorization") authHeader: String, @Query("gymId") gymId: String, @Body request: CreateFrequencyBasedChallengeRequest): Response<Unit>
+  @POST("/challenges/frequency-based")
+  suspend fun createFrequencyBasedChallenge(
+    @Header("Authorization") authHeader: String,
+    @Query("gymId") gymId: String,
+    @Body request: CreateFrequencyBasedChallengeRequest,
+  ): Response<Unit>
 
-    @POST("/challenges/invite-based")
-    suspend fun createInviteFriendChallenge(@Header("Authorization") authHeader: String, @Query("gymId") gymId: String, @Body request: CreateInviteFriendChallengeRequest): Response<Unit>
+  @POST("/challenges/invite-based")
+  suspend fun createInviteFriendChallenge(
+    @Header("Authorization") authHeader: String,
+    @Query("gymId") gymId: String,
+    @Body request: CreateInviteFriendChallengeRequest,
+  ): Response<Unit>
 
-    @DELETE("/challenges/{challengeId}")
-    suspend fun deleteChallenge(@Header("Authorization") authHeader: String, @Path("challengeId") challengeId: String): Response<Unit>
+  @DELETE("/challenges/{challengeId}")
+  suspend fun deleteChallenge(
+    @Header("Authorization") authHeader: String,
+    @Path("challengeId") challengeId: String,
+  ): Response<Unit>
 
-    @PUT("/challenges/{challengeId}")
-    suspend fun updateChallenge(@Header("Authorization") authHeader: String, @Path("challengeId") challengeId: String, @Body request: UpdateChallengeRequest): Response<Unit>
+  @PUT("/challenges/{challengeId}")
+  suspend fun updateChallenge(
+    @Header("Authorization") authHeader: String,
+    @Path("challengeId") challengeId: String,
+    @Body request: UpdateChallengeRequest,
+  ): Response<Unit>
 
-    @GET("/challenges/unclaimed")
-    suspend fun fetchUnclaimedChallenges(@Header("Authorization") authHeader: String, @Query("gymId") gymId: String): List<ChallengeDto>
+  @GET("/challenges/unclaimed")
+  suspend fun fetchUnclaimedChallenges(
+    @Header("Authorization") authHeader: String,
+    @Query("gymId") gymId: String,
+  ): List<ChallengeDto>
 
-    @GET("/challenges/points")
-    suspend fun fetchPoints(@Header("Authorization") authHeader: String, @Query("gymId") gymId: String): Int
+  @GET("/challenges/points")
+  suspend fun fetchPoints(
+    @Header("Authorization") authHeader: String,
+    @Query("gymId") gymId: String,
+  ): Int
 
-    @POST("/challenges/{challengeId}/claim")
-    suspend fun claimChallenge(@Header("Authorization") authHeader: String, @Path("challengeId") challengeId: String): Response<Unit>
+  @POST("/challenges/{challengeId}/claim")
+  suspend fun claimChallenge(
+    @Header("Authorization") authHeader: String,
+    @Path("challengeId") challengeId: String,
+  ): Response<Unit>
 
-    @GET("/gyms/{gymId}/gymVisits/heatMap")
-    suspend fun fetchHeatmapData(@Header("Authorization") authHeader: String, @Path("gymId") gymId: String): List<VisitCountByDay>
+  @GET("/gyms/{gymId}/gymVisits/heatMap")
+  suspend fun fetchHeatmapData(
+    @Header("Authorization") authHeader: String,
+    @Path("gymId") gymId: String,
+  ): List<VisitCountByDay>
 }
